07f0d978dba80aac536ae0772084f6da4cfdb8ba
Semyon <b-semen-b@mail.ru>
2022-03-21 13:25:37 +0300

    hotxfix

---
+++ request_context.go
@@ -0,0 +1,11 @@
+package jira
+
+import (
+	"context"
+	"io"
+	"net/http"
+)
+
+func newRequestWithContext(ctx context.Context, method, url string, body io.Reader) (*http.Request, error) {
+	return http.NewRequestWithContext(ctx, method, url, body)
+}
